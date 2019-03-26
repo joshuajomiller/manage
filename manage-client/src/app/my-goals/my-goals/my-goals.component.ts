31c7cb3fb8c428c2af9df4c27973a34c637e69be
joshuajomiller <joshuajosephmiller@gmail.com>
2019-03-27 00:11:46 +0200

    Fake model in goals, css and status change action

--- manage-client/src/app/my-goals/my-goals/my-goals.component.ts
+++ manage-client/src/app/my-goals/my-goals/my-goals.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyGoalsComponent implements OnInit {
 
-  constructor() { }
+  model: any;
+
+  constructor() {
+    this.model = [];
+    const modelObj = {
+      title: "Goal Title",
+      description: "Here is a description for your goal that I would really like you to do.",
+      tags: ["personal", "growth"],
+      status: "open"
+    };
+    for (let x=0; x < 15; x++){
+      this.model.push({...modelObj});
+    }
+  }
 
   ngOnInit() {
   }
 
+  toggleTaskStatus(goal){
+    goal.status = (goal.status === "open" ? 'pending' : 'open');
+  }
 }
